dd0f564edd1ea6b55cfdf3588271f7ef3e848a0b
Prajwal M <122041305+Prajwal-M-Rao@users.noreply.github.com>
2025-05-13 15:27:37 +0530

    urls.py

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path('', include('resto.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    
-
 ]
