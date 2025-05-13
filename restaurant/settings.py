e8b75066cfb1ddb64e9368acee7059011ef823f8
Prajwal M <122041305+Prajwal-M-Rao@users.noreply.github.com>
2025-05-13 15:28:33 +0530

    settings.py

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -121,7 +121,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -139,7 +138,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
